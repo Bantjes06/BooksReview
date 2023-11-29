8530b53c05a947a97f56c99206343373f33fa8df
Johan <Bantjes06@gmail.com>
2023-11-29 17:15:54 +0200

    Home page working and add review, UI to be done and more functionality

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import ejs from "ejs";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "books",
+  password: "0616610022",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let books = [];
+
+app.get("/", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM book_details ORDER BY id ASC");
+      books = result.rows;
+      console.log(books);
+  
+      res.render("index.ejs", {
+        books: books
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+app.get("/new", async (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/submit", async (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const review = req.body.review;
+  const isbn = req.body.isbn;
+  const rating = req.body.rating;
+  const readDate = req.body.readDate;
+  const link = req.body.link;
+
+  const result = await db.query(
+    "INSERT INTO book_details (title, description, review, isbn, rating, read_date, link) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *;",
+    [title, description, review, isbn, rating, readDate, link]
+  );
+
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on Port ${port}`);
+})
