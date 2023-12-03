bb12e44aa764c49778fd92335d6484b988bb3708
Johan <Bantjes06@gmail.com>
2023-12-03 15:45:17 +0200

    Edit, delete and sorting functionality done. UI to be polished and some logic.

--- index.js
+++ index.js
@@ -34,6 +34,34 @@ app.get("/", async (req, res) => {
     }
   });
 
+  app.get("/latest", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM book_details ORDER BY id DESC");
+      books = result.rows;
+      console.log(books);
+  
+      res.render("index.ejs", {
+        books: books
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  app.get("/rating", async (req, res) => {
+    try {
+      const result = await db.query("SELECT * FROM book_details ORDER BY rating DESC");
+      books = result.rows;
+      console.log(books);
+  
+      res.render("index.ejs", {
+        books: books
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
 app.get("/new", async (req, res) => {
   res.render("new.ejs");
 });
@@ -67,6 +95,46 @@ app.post('/read-review', async (req, res) => {
   }
 });
 
+app.post('/edit', async (req, res) => {
+  try {
+      const id = req.body.editBookId;
+      const result = await db.query('SELECT * FROM book_details WHERE id = $1', [id]);
+      const bookDetails = result.rows[0];
+      res.render('edit_review.ejs', { book: bookDetails });
+  } catch (err){
+      console.error(err);
+      res.status(500).send('Server error');
+  }
+});
+
+app.post("/update", async (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const review = req.body.review;
+  const isbn = req.body.isbn;
+  const link = req.body.link;
+  const id = req.body.updateBookId;
+
+  try{
+    const result = await db.query(
+      "UPDATE book_details SET title = $1, description = $2, review = $3, isbn = $4, link = $5 WHERE id = $6",
+      [title, description, review, isbn, link, id]
+    );
+    res.redirect("/");
+  } catch(err) {
+    console.log(err);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteBookId;
+  try {
+    await db.query("DELETE FROM book_details WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on Port ${port}`);
